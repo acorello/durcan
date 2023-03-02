d2a82f7ce2bb7398d5678553fc0415ec54960bcd
am <am@acorello.it>
2023-03-02 18:38:25 +0100

    Add dailyAverage option

--- main.go
+++ main.go
@@ -1,21 +1,38 @@
 package main
 
 import (
+	"flag"
 	"fmt"
-	"os"
 	"time"
 )
 
+var overDaysFlag = flag.Int("overdays", 0, "calculate the average time spent over given days")
+
 func main() {
-	inputs := os.Args[1:]
+	flag.Parse()
+	inputs := flag.Args()
+	overDays := *overDaysFlag
+
 	if len(inputs) == 1 {
 		input := inputs[0]
 		duration, _ := time.ParseDuration(input)
-		fmt.Println(duration)
+		fmt.Print(duration)
+		dailyAvg := dailyAverageMessage(overDays, duration)
+		fmt.Println(dailyAvg)
 		return
 	}
 	for _, input := range inputs {
 		duration, _ := time.ParseDuration(input)
 		fmt.Printf("%-10s = %9s\n", input, duration)
+		dailyAvg := dailyAverageMessage(overDays, duration)
+		fmt.Println(dailyAvg)
 	}
+}
+
+func dailyAverageMessage(overDays int, duration time.Duration) (dailyAverage string) {
+	if overDays > 0 {
+		average := duration / time.Duration(overDays)
+		dailyAverage = fmt.Sprintln("\tdaily average:", average)
+	}
+	return
 }
